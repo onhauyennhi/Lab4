4b3ed6d134501095e9f1e2eba435d29becea3e45
Nhi On <94783814+onhauyennhi@users.noreply.github.com>
2022-09-30 10:54:45 +0700

    Add files via upload

---
+++ Login_Form/Login_Form/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Login_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbUsername = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.cbRemember = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // txbUsername
+            // 
+            this.txbUsername.AccessibleName = "Username";
+            this.txbUsername.Location = new System.Drawing.Point(274, 89);
+            this.txbUsername.Multiline = true;
+            this.txbUsername.Name = "txbUsername";
+            this.txbUsername.Size = new System.Drawing.Size(300, 39);
+            this.txbUsername.TabIndex = 2;
+            this.txbUsername.TextChanged += new System.EventHandler(this.txbUsername_TextChanged);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(137, 339);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(192, 53);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Location = new System.Drawing.Point(274, 190);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.Size = new System.Drawing.Size(300, 26);
+            this.txbPassword.TabIndex = 6;
+            this.txbPassword.UseSystemPasswordChar = true;
+            // 
+            // cbRemember
+            // 
+            this.cbRemember.AutoSize = true;
+            this.cbRemember.Location = new System.Drawing.Point(137, 282);
+            this.cbRemember.Name = "cbRemember";
+            this.cbRemember.Size = new System.Drawing.Size(140, 24);
+            this.cbRemember.TabIndex = 7;
+            this.cbRemember.Text = "Remember Me";
+            this.cbRemember.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(380, 339);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 53);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbRemember);
+            this.Controls.Add(this.txbPassword);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txbUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbUsername;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txbPassword;
+        private System.Windows.Forms.CheckBox cbRemember;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Login_Form/Login_Form/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            loadData();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            if (txbUsername.Text.Equals(""))
+            {
+                MessageBox.Show("Không được để trống tên tài khoản!");
+            }else if (txbPassword.Text.Equals(""))
+            {
+                MessageBox.Show("Không được để trống mật khẩu!");
+            }
+            else if (txbPassword.Text.Length < 8)
+            {
+                MessageBox.Show("Mật khẩu phải có ít nhất 8 kí tự!");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thành công!");
+                if (cbRemember.Checked)
+                {
+                    Properties.Settings.Default.Username = txbUsername.Text;
+                    Properties.Settings.Default.Password = txbPassword.Text;
+                    Properties.Settings.Default.Remember = true;
+                    Properties.Settings.Default.Save();
+                }
+                else if (!cbRemember.Checked)
+                {
+                    Properties.Settings.Default.Username = "";
+                    Properties.Settings.Default.Password = "";
+                    Properties.Settings.Default.Remember = false;
+
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+        private void loadData()
+        {
+            if (Properties.Settings.Default.Username !=null)
+            {
+                txbUsername.Text = Properties.Settings.Default.Username;
+            }
+            if (Properties.Settings.Default.Password != null)
+            {
+                txbPassword.Text = Properties.Settings.Default.Password;
+            }
+            if (Properties.Settings.Default.Remember==true)
+            {
+                cbRemember.Checked = true;
+            }
+        }
+        private void txbUsername_TextChanged(object sender, EventArgs e) 
+        {
+
+        }
+
+        private void rdbRemember_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txbUsername.Clear();
+            txbPassword.Clear();
+            cbRemember.Checked = false;
+        }
+    }
+}
