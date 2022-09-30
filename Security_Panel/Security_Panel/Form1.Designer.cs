580745be9322f3a0215a2522b284d2f5c14528e0
Nhi On <94783814+onhauyennhi@users.noreply.github.com>
2022-09-30 10:52:00 +0700

    Add files via upload

---
+++ Security_Panel/Security_Panel/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Security_Panel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSecurityCode = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbxAccessLog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 85);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Security Code:";
+            // 
+            // txtSecurityCode
+            // 
+            this.txtSecurityCode.Location = new System.Drawing.Point(243, 82);
+            this.txtSecurityCode.Name = "txtSecurityCode";
+            this.txtSecurityCode.PasswordChar = '*';
+            this.txtSecurityCode.ReadOnly = true;
+            this.txtSecurityCode.Size = new System.Drawing.Size(221, 26);
+            this.txtSecurityCode.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 55);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(217, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 55);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(317, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 55);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(120, 234);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 55);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(217, 234);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(56, 55);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(317, 234);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(56, 55);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(120, 316);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(56, 55);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(217, 316);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(56, 55);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(317, 316);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(56, 55);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(408, 143);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(56, 55);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "C";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(408, 234);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(56, 55);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "#";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(408, 316);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(56, 55);
+            this.button0.TabIndex = 13;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 404);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Access log:";
+            // 
+            // lbxAccessLog
+            // 
+            this.lbxAccessLog.FormattingEnabled = true;
+            this.lbxAccessLog.ItemHeight = 20;
+            this.lbxAccessLog.Location = new System.Drawing.Point(120, 462);
+            this.lbxAccessLog.Name = "lbxAccessLog";
+            this.lbxAccessLog.Size = new System.Drawing.Size(344, 184);
+            this.lbxAccessLog.TabIndex = 15;
+            this.lbxAccessLog.SelectedIndexChanged += new System.EventHandler(this.lbxAccessLog_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 686);
+            this.Controls.Add(this.lbxAccessLog);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSecurityCode);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Security Panel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSecurityCode;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbxAccessLog;
+    }
+}
+
---
+++ Security_Panel/Security_Panel/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Deployment.Application;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Security_Panel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string status = "";
+        string group = "";
+        string path = @"C:\Users\Naomi On\Downloads\Năm 3 HK1\AI\Security_Panel\Login_Log.txt";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtSecurityCode.Text += button1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtSecurityCode.Text += button2.Text;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtSecurityCode.Text += button3.Text;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txtSecurityCode.Text += button4.Text;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtSecurityCode.Text += button5.Text;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            txtSecurityCode.Text += button6.Text;
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            txtSecurityCode.Text += button7.Text;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            txtSecurityCode.Text += button8.Text;
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            txtSecurityCode.Text += button9.Text;
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            txtSecurityCode.Text += button0.Text;
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txtSecurityCode.Clear();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            showLog();
+        }
+
+        private void lbxAccessLog_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                showLog();
+            }
+            else if (e.KeyCode == Keys.D0)
+            {
+                txtSecurityCode.Text += "0";
+            }
+            else if (e.KeyCode == Keys.D1)
+            {
+                txtSecurityCode.Text += "1";
+            }
+            else if (e.KeyCode == Keys.D2)
+            {
+                txtSecurityCode.Text += "2";
+            }
+            else if (e.KeyCode == Keys.D3)
+            {
+                txtSecurityCode.Text += "3";
+            }
+            else if (e.KeyCode == Keys.D4)
+            {
+                txtSecurityCode.Text += "4";
+            }
+            else if (e.KeyCode == Keys.D5)
+            {
+                txtSecurityCode.Text += "5";
+            }
+            else if (e.KeyCode == Keys.D6)
+            {
+                txtSecurityCode.Text += "6";
+            }
+            else if (e.KeyCode == Keys.D7)
+            {
+                txtSecurityCode.Text += "7";
+            }
+            else if (e.KeyCode == Keys.D8)
+            {
+                txtSecurityCode.Text += "8";
+            }
+            else if (e.KeyCode == Keys.D9)
+            {
+                txtSecurityCode.Text += "9";
+            }
+            else if (e.KeyCode == Keys.Back)
+            {
+                txtSecurityCode.Clear();
+            }
+        }
+        private void showLog()
+        {
+            int code = Convert.ToInt32(txtSecurityCode.Text);
+            switch (code)
+            {
+                case 1645:
+                    status = "granted";
+                    group = "Technicians";
+                    lbxAccessLog.Items.Add(" " + DateTime.Now.ToString() + " " + group);
+                    break;
+                case 1689:
+                    status = "granted";
+                    group = "Technicians";
+                    lbxAccessLog.Items.Add(" " + DateTime.Now.ToString() + " " + group);
+
+                    break;
+                case 8345:
+                    status = "granted";
+                    group = "Custodians";
+                    lbxAccessLog.Items.Add(" " + DateTime.Now.ToString() + " " + group);
+
+                    break;
+                case 9998:
+                    status = "granted";
+                    group = "Scientist";
+                    lbxAccessLog.Items.Add(" " + DateTime.Now.ToString() + " " + group);
+
+                    break;
+                case 1006:
+                    status = "granted";
+                    group = "Scientist";
+                    lbxAccessLog.Items.Add(" " + DateTime.Now.ToString() + " " + group);
+
+                    break;
+                case 1008:
+                    status = "granted";
+                    group = "Scientist";
+                    lbxAccessLog.Items.Add(" " + DateTime.Now.ToString() + " " + group);
+                    break;
+                default:
+                    status = "denied";
+                    lbxAccessLog.Items.Add(" " + DateTime.Now.ToString() + " Restricted Access!");
+                    break;
+            }
+            WriteToFile();
+        }
+        private void WriteToFile()
+        {
+            if (status == "granted")
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(" " + DateTime.Now.ToString() + " " + group);
+                }
+            }
+            else if (status == "denied")
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(" " + DateTime.Now.ToString() + " Restricted Access! ");
+                }
+            }
+        }
+    }
+}
